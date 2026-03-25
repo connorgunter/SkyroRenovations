b746e349ac54dfd550ac785914f2ed7b51b256bd
Connor Gunter <bigc5674@gmail.com>
2026-03-25 12:59:07 -0400

    added seals for BBB and Duns

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,36 @@
 import "./Footer.css";
+import dunsBadge from "../../assets/dunsseal.png";
 
 const Footer = () => {
   return (
     <footer className="site-footer">
+      <div className="trust-badges">
+        {/* BBB */}
+        <a
+          href="https://www.bbb.org/us/fl/jensen-beach/profile/building-contractors/skyro-renovations-llc-0633-90356635/#sealclick"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="https://seal-seflorida.bbb.org/seals/blue-seal-293-61-bbb-90356635.png"
+            alt="BBB Accredited Business"
+          />
+        </a>
+
+        {/* DUNS */}
+        <a
+          href="https://www.dnb.com/business-directory/company-profiles.skyro_renovations_llc.7a6a13c646a8777770f134efa120ddf0.html"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={dunsBadge}
+            className="duns-image"
+            alt="D-U-N-S Registered"
+          />
+        </a>
+      </div>
+
       <p>
         © 2025 Skyro Renovations LLC. Site by{" "}
         <a
@@ -17,4 +45,5 @@ const Footer = () => {
     </footer>
   );
 };
+
 export default Footer;
